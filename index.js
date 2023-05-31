cbf054010e6a777e4bb0fae58341bf3d93d75f01
angel <ANGEL.GARCIA0038@GMAIL.COM>
2023-05-31 15:26:57 -0700

    added the rest of the questions required

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
 const generateLogo = require('./utils/generateLogo')
-
+// prompted questions for logo creation
 const questions = [
     {
         type: "list",
-        message: "what shape svg?",
+        message: "what shape would you like your svg?",
         name: "shape",
         choices: [
             "Triangle",
@@ -14,4 +14,22 @@ const questions = [
             "circle"
         ]
     },
-]
+
+    {
+        type: "input",
+        message: "What color would you like the shape to be (Or a hexadecimal number)?",
+        name: "shape_color"
+    },
+
+    {
+        type: "input",
+        message: "Please type in a character set of 3 letters.",
+        name: "text",
+    },
+
+    {
+        type: "input",
+        message: "what color would you like the text to be?",
+        name: "text_color"
+    },
+];

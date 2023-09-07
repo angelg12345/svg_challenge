16ac07f55626f3dc1c7a4bc7119aea66efa0eda8
angel <ANGEL.GARCIA0038@GMAIL.COM>
2023-09-06 18:28:29 -0700

    added some comments.

--- utils/generateLogo.js
+++ utils/generateLogo.js
@@ -1,9 +1,11 @@
-
+// Importing required shape classes
 const Circle = require("../lib/Circle")
 const Triangle = require("../lib/Triangle")
 const Square = require("../lib/Square")
+// function to generate the logo based on the user
 function generateLogo(data){
     let shape = undefined
+    // gives the shape based on what the user picks.
     if (data.shape === 'Triangle') {
         shape = new Triangle(data.shapeColor, data.text, data.textColor)
     } else if (data.shape === 'Square') {
@@ -11,6 +13,8 @@ function generateLogo(data){
     } else {
         shape = new Circle(data.shapeColor, data.text, data.textColor)
     }
+    // Returns the shape and rendurs svg
     return shape.render();
 }
+// export to make it available to rest of app
 module.exports = generateLogo;
